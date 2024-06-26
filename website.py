2e57386f91df4d80a50be292ccdcbf7d69575c4e
Pradeesh89 <132539023+Pradeesh890@users.noreply.github.com>
2024-06-26 22:46:07 +0530

    Update website.py

--- website.py
+++ website.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
 def main():
-
-    st.title('BOSTON HOUSE')
-    st.markdown('<a href="https://colab.research.google.com/drive/1AjWyv9DnXcx7Oh5sOfBsjPjAtzjzysi5#scrollTo=QOBrLyysPBLV" target="_blank"><h1 style="color:blue;">Boston House</h1></a>', unsafe_allow_html=True)
-    
+    st.markdown('<a href="https://colab.research.google.com/drive/1AjWyv9DnXcx7Oh5sOfBsjPjAtzjzysi5?usp=sharing" target="_blank"><h1 style="color:blue;">Boston House</h1></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://colab.research.google.com/drive/1lw3EaXVISCJlLf4AbiE1o7V42-P7Z3iZ?usp=sharing" target="_blank"><h1 style="color:blue;">Bivariate Data Analysis and Visualization</h1></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://colab.research.google.com/drive/1G-Vh4xrTngs9bHP6wWGQg71EsEnnjdw1?usp=sharing" target="_blank"><h1 style="color:blue;">IRIS FLOWER CLASSIFICATION</h1></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://colab.research.google.com/drive/102xfXgyfaJIhN37i5lKBRJnRaxi1zrS9?usp=sharing" target="_blank"><h1 style="color:blue;">univariate or vehicle problem</h1></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://colab.research.google.com/drive/1N6CrR0XOfpt6vbD2joGz-EzYWvxdPhsX?usp=sharing" target="_blank"><h1 style="color:blue;">EDA or Credit card problem</h1></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://colab.research.google.com/drive/1fyKHeQ9qVtKmDbtatiimwlipeurCvldz?usp=sharing" target="_blank"><h1 style="color:blue;">Forex Data Analysis and Visualization</h1></a>', unsafe_allow_html=True)
+    st.markdown('<a href="https://colab.research.google.com/drive/1gwOcGmudVEVuqcx012MbhSZNbGALatoK?usp=sharing" target="_blank"><h1 style="color:blue;">Event management system</h1></a>', unsafe_allow_html=True)
 if __name__ == '__main__':
     main()
